a862cc24bbfc34299c39be6bc0bd78fb73c8c817
GEKO-3 <149662809+GEKO-3@users.noreply.github.com>
2025-07-25 12:56:53 +0500

    links hidden for g forms and g sheets

---
+++ config.js
@@ -0,0 +1,80 @@
+// Rhythm Boduberu Configuration - Obfuscated
+(function() {
+    'use strict';
+    
+    const _0x4a5b = [
+        'aHR0cHM6Ly9kb2NzLmdvb2dsZS5jb20vZm9ybXMvZC9lLzFGQUlwUUxTY2JwNUE2SFhzVm81XzhDSkVIdlpidEJfamFObnkxa0s2X1lQa1RvUXY0TlA4cmcvZm9ybVJlc3BvbnNl',
+        'ZW50cnkuMTAwMzU3MzIxMw==',
+        'ZW50cnkuNzM3OTM5MDAx',
+        'ZW50cnkuMTgxMDczMjk2OQ==',
+        'ZW50cnkuNTI3Nzk1NjY0',
+        'ZW50cnkuMjg5MDM0Mw==',
+        'aHR0cHM6Ly9kb2NzLmdvb2dsZS5jb20vc3ByZWFkc2hlZXRzL2QvZS8yUEFDWC0xdlFnM0phOHZGXzU2Zks1VGNpOE5kcFBxemlKOS1sR1BhUjFLbW5aTjJ6b0JDeXIwWVVjWkZIakdsYnNpRzdxbXJKZHNVRFJXSzFRdXQ5Ri9wdWI/Z2lkPTIwNDA5MDE5MTAmc2luZ2xlPXRydWUmb3V0cHV0PWNzdg==',
+        'aHR0cHM6Ly9kb2NzLmdvb2dsZS5jb20vc3ByZWFkc2hlZXRzL2QvZS8yUEFDWC0xdlJZZ2hWUUxwelNhdm5FdktSV0tZYnFhZE9EVjF5bDcxUnFNWXNSbERHaUpZcWpXaDM5OEo3LWNnbGpMWW9qMXlMQUxQa0tEZmtzZFBlQy9wdWI/b3V0cHV0PWNzdg==',
+        'aHR0cHM6Ly9kb2NzLmdvb2dsZS5jb20vZm9ybXMvZC9lLzFGQUlwUUxTZmVMTkJ2TDhLZThTUFZSNmhuaWhscnFvWExMZFc0VElCNS1iVVBsaXVUVGFQOUZRL2Zvcm1SZXNwb25zZQ==',
+        'ZW50cnkuMTUxODczMjY2NA==',
+        'ZW50cnkuNTgyMzM5NjIz',
+        'aHR0cHM6Ly9kb2NzLmdvb2dsZS5jb20vc3ByZWFkc2hlZXRzL2QvZS8yUEFDWC0xdlJ4VWxNS0k5RmRxZlRIakQzeVREaXV3bGhId3pyZm9WRUtYc0EwWmZUcFFMR25fU0d2TTJ1SnV6NlVSQTNfR1hSRkJ2b2Zqa2EtNGZKRi9wdWI/b3V0cHV0PWNzdg==',
+        'aHR0cHM6Ly9kb2NzLmdvb2dsZS5jb20vc3ByZWFkc2hlZXRzL2QvZS8yUEFDWC0xdlNvckVBaHg4TG1MRDFtOG1LREt1Wk1ZV2xFQ0EyNFFxRll0N2FfczVCMlRheFIxTjlYYkhIRXVjS1VpVVlDTHBWRjBYVktsMTRMWWE2bC9wdWI/b3V0cHV0PWNzdg==',
+        'aHR0cHM6Ly9kb2NzLmdvb2dsZS5jb20vZm9ybXMvZC9lLzFGQUlwUUxTY29qRVZiaDZKTEllcHd3UXhKdElBQjdCbUlUUC12cDZQRFk3SkZTNTFacnFyRmcvZm9ybVJlc3BvbnNl',
+        'ZW50cnkuMTg4NjA4NTMyNQ==',
+        'aHR0cHM6Ly9kb2NzLmdvb2dsZS5jb20vZm9ybXMvZC9lLzFGQUlwUUxTY1dqWW5pdEZKWmwzWWktZTlyYVNBU2dBWHpsYThHb2c2X1FVMFF6ZlpaWkxUUS9mb3JtUmVzcG9uc2U=',
+        'ZW50cnkuMTM2NTQwNjc0Nw==',
+        'ZW50cnkuMTgwNDk5MTEyNQ==',
+        'ZW50cnkuNTc4NDM3ODUz',
+        'ZW50cnkuOTQwOTM2ODUz'
+    ];
+    
+    function _0x2c4f(str) {
+        try {
+            return atob(str);
+        } catch(e) {
+            return '';
+        }
+    }
+    
+    window['\x52\x68\x79\x74\x68\x6d\x43\x6f\x6e\x66\x69\x67'] = {
+        get applicationForm() { return _0x2c4f(_0x4a5b[0]); },
+        get applicationEntries() {
+            return {
+                email: _0x2c4f(_0x4a5b[1]),
+                name: _0x2c4f(_0x4a5b[2]),
+                birthday: _0x2c4f(_0x4a5b[3]),
+                contact: _0x2c4f(_0x4a5b[4]),
+                location: _0x2c4f(_0x4a5b[5])
+            };
+        },
+        
+        get applicationsSheet() { return _0x2c4f(_0x4a5b[6]); },
+        get applicationsStatusSheet() { return _0x2c4f(_0x4a5b[7]); },
+        get applicationsStatusForm() { return _0x2c4f(_0x4a5b[8]); },
+        get applicationsStatusEntries() {
+            return {
+                uid: _0x2c4f(_0x4a5b[9]),
+                status: _0x2c4f(_0x4a5b[10])
+            };
+        },
+        
+        get sponsorCallbackSheet() { return _0x2c4f(_0x4a5b[11]); },
+        get sponsorCallbackStatusSheet() { return _0x2c4f(_0x4a5b[12]); },
+        get sponsorCallbackForm() { return _0x2c4f(_0x4a5b[13]); },
+        get sponsorCallbackEntries() {
+            return {
+                uid: _0x2c4f(_0x4a5b[14])
+            };
+        },
+        
+        get sponsorProposalForm() { return _0x2c4f(_0x4a5b[15]); },
+        get sponsorProposalEntries() {
+            return {
+                name: _0x2c4f(_0x4a5b[16]),
+                phone: _0x2c4f(_0x4a5b[17]),
+                company: _0x2c4f(_0x4a5b[18]),
+                interest: _0x2c4f(_0x4a5b[19])
+            };
+        }
+    };
+    
+    delete window._0x4a5b;
+    delete window._0x2c4f;
+})();
---
+++ lyrics-showlist-config.js
@@ -0,0 +1,64 @@
+// Rhythm Boduberu Lyrics & Showlist Configuration - Obfuscated
+(function() {
+    'use strict';
+    
+    const _0x7e8c = [
+        'aHR0cHM6Ly9kb2NzLmdvb2dsZS5jb20vc3ByZWFkc2hlZXRzL2QvZS8yUEFDWC0xdlNWdTVmOXgyZi11Mmhha0xfMXBaeV9oYmVweHRuRmY3TVJXVzlMekd1OFNDcV95UWl0aFBETUJWd1dKZmdjeFJYOVVlY09SaTNlVmpSZC9wdWI/b3V0cHV0PWNzdg==',
+        'aHR0cHM6Ly9kb2NzLmdvb2dsZS5jb20vZm9ybXMvZC9lLzFGQUlwUUxTZG5Kb1JkaHp3VWQ3QmpJQnhwaWZ4QTB0aFFWQVpZVVhCMnZGZnJkbU9oamdZZGx3L2Zvcm1SZXNwb25zZQ==',
+        'ZW50cnkuMTk5Nzc5ODEwNQ==',
+        'ZW50cnkuNTQzNzY1OTMz',
+        'ZW50cnkuMzIwMDQxNDM3',
+        'ZW50cnkuMjY5NjQxMTAw',
+        'aHR0cHM6Ly9kb2NzLmdvb2dsZS5jb20vc3ByZWFkc2hlZXRzL2QvZS8yUEFDWC0xdlQydTRGdTJmMGJZRl9Ub1BVLWRpZ0QxamJxTnNMN0RPSnBCX2tYbFdUc25YY2dldHN2Y0h1aG5NcXRGLWdDRmlfUG5TaVd6VXdiTjNwNi9wdWI/b3V0cHV0PWNzdg==',
+        'aHR0cHM6Ly9kb2NzLmdvb2dsZS5jb20vc3ByZWFkc2hlZXRzL2QvZS8yUEFDWC0xdlFkS2xDRkRjMUlFUVhFZ2prWDFVTHBHTGpXaXRSemFIOFdzYTFmTkdxR2JYa011U2hUTm1URHNrMWZMNkY4STNEMDIyem4ySFVJckN1eC9wdWI/b3V0cHV0PWNzdg==',
+        'aHR0cHM6Ly9kb2NzLmdvb2dsZS5jb20vZm9ybXMvZC9lLzFGQUlwUUxTZjVlWExLQXdnUzNIbm1iUEN3cHNGaU5LZFVVVU45eWdwZXByVi1za0RQWGJSemxnL2Zvcm1SZXNwb25zZQ==',
+        'ZW50cnkuMTk1OTg3OTYyMQ==',
+        'ZW50cnkuOTU4NDgyNjQz',
+        'ZW50cnkuMTU3MzIwNzUxOA==',
+        'aHR0cHM6Ly9kb2NzLmdvb2dsZS5jb20vZm9ybXMvZC9lLzFGQUlwUUxTZmNxYW50T0x3eGQ0Y2NEOWJfSHRTQ3dHYkVWdHM2eGtJOHdiMWpvZnlxUEk4QW1RL2Zvcm1SZXNwb25zZQ==',
+        'ZW50cnkuMjA2NjgyNzUxNg==',
+        'ZW50cnkuOTkzMzcwNjIx'
+    ];
+    
+    function _0x9d1a(str) {
+        try {
+            return atob(str);
+        } catch(e) {
+            return '';
+        }
+    }
+    
+    window['\x52\x68\x79\x74\x68\x6d\x4c\x79\x72\x69\x63\x73\x53\x68\x6f\x77\x43\x6f\x6e\x66\x69\x67'] = {
+        get lyricsSheet() { return _0x9d1a(_0x7e8c[0]); },
+        get lyricsForm() { return _0x9d1a(_0x7e8c[1]); },
+        get lyricsEntries() {
+            return {
+                id: _0x9d1a(_0x7e8c[2]),
+                name: _0x9d1a(_0x7e8c[3]),
+                genre: _0x9d1a(_0x7e8c[4]),
+                lyrics: _0x9d1a(_0x7e8c[5])
+            };
+        },
+        
+        get showListSheet() { return _0x9d1a(_0x7e8c[6]); },
+        get cancelSheet() { return _0x9d1a(_0x7e8c[7]); },
+        get showListForm() { return _0x9d1a(_0x7e8c[8]); },
+        get showListEntries() {
+            return {
+                uid: _0x9d1a(_0x7e8c[9]),
+                data: _0x9d1a(_0x7e8c[10]),
+                name: _0x9d1a(_0x7e8c[11])
+            };
+        },
+        get cancelForm() { return _0x9d1a(_0x7e8c[12]); },
+        get cancelEntries() {
+            return {
+                uid: _0x9d1a(_0x7e8c[13]),
+                status: _0x9d1a(_0x7e8c[14])
+            };
+        }
+    };
+    
+    delete window._0x7e8c;
+    delete window._0x9d1a;
+})();
